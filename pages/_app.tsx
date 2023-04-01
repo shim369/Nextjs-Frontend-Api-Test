34aee7302d6c284d29fcd9e7a44c325b46428892
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-04-01 17:00:47 +0900

    add security headers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,20 @@
-import '@/styles/globals.css';
-
+import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { NextPageContext } from "next"
 
-export default function App({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
+
+MyApp.getInitialProps = async ({ Component, ctx }: { Component: any, ctx: NextPageContext }) => {
+  const { res } = ctx;
+  if (res) {
+    res.setHeader("X-Content-Type-Options", "nosniff");
+    res.setHeader("X-Frame-Options", "DENY");
+    res.setHeader("X-XSS-Protection", "1; mode=block");
+  }
+  const pageProps = Component.getInitialProps ? await Component.getInitialProps(ctx) : {};
+  return { pageProps };
+};
+
+export default MyApp;
